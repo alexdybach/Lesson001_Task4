8c1f3a44d0f5a25dbe5addb19260b03b06633a20
Олександр Дибач <alex.dybach.2014@i.ua>
2020-10-27 12:55:06 +0200

    Done lesson 1 task 4

---
+++ Less001_Task4/Less001_Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Less001_Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float float_division = 1.0f/ 2.0f;
+            double double_division = 1.0 / 2.0;
+            Console.WriteLine("float:   "+float_division);
+            Console.WriteLine("double:   " + double_division);
+
+        }
+    }
+}
